533dbc6dc85e5ee902a840f4a56bc423a5296311
parr0x <roberparro@gmail.com>
2019-06-01 13:55:15 +0200

    Clean up

--- Descarga_Funtion.py
+++ Descarga_Funtion.py
@@ -3,38 +3,27 @@ import json
 import sqlite3
 import urllib
 
-def descarga(datei, datef, ind):
-    print(datei, datef, ind)
 
-
-    url = "https://api.esios.ree.es/indicators/{indicator}?{query}"
-
-
-    Fecha_Inicial = datei
-    Fecha_final = datef
-    aurl = "?start_date="
-    b = "T00:00:00&end_date="
-    t = "&time_trunc=hour"
-    i = ind
-    indicadores = len(i)
+def get_values(start_date, end_date, indicators):
+    print(start_date, end_date, indicators)
+    urltemplate = "https://api.esios.ree.es/indicators/{indicator}?{query}"
     headers = {'content-Type': 'application/json',
                'Authorization': 'Token token=4ae31570833b25918e54ccbb20fe36f7ccf61767314764a65b9f51d0158e0575'}
-    for j in range(indicadores):
-        query=urllib.parse.urlencode({
-            'start_date': Fecha_Inicial,
-            'end_date': Fecha_final,
+    for an_indicator in indicators:
+        query = urllib.parse.urlencode({
+            'start_date': start_date,
+            'end_date': end_date,
             'time_trunc': 'hour'
         })
-        Miurl = url.format(indicator=str(i[j]), query=query)
-        print(Miurl)
-        response = requests.get(Miurl, headers=headers, stream=True)
+        url = urltemplate.format(indicator=str(an_indicator), query=query)
+        print(url)
+        response = requests.get(url, headers=headers, stream=True)
         print(response)
         data_dict = json.loads(response.content.decode('UTF-8'))
         _save_data(_interesting_values(data_dict), 'data.db')
 
 
 def _save_data(data, db_path):
-
     conn = sqlite3.connect(db_path)
     conn.executemany('INSERT OR IGNORE INTO PBFSchedule VALUES (?,?,?)', data)
     conn.commit()
@@ -46,4 +35,6 @@ def _interesting_values(source):
     for record in source["indicator"]["values"]:
         yield (name, record["datetime"], record["value"])
 
-descarga('2018-05-12', '2018-05-18', [10167])
+
+indicators = [10167,10131,10122,10113,10104,10095,10086,10077,10074,10073,10064,25,20,14,15,9,2,1,3,4]
+get_values('2019-06-01', '2019-06-02', indicators)
